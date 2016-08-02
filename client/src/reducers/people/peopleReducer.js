606892313ed6a4e7b0c798cad15fd393a40ce742
Sandi Barr <sandi.barr@objectpartners.com>
2016-08-02 15:17:14 -0500

    splits people reducer to byName and allNames to fix duplicate entries

--- client/src/reducers/people/peopleReducer.js
+++ client/src/reducers/people/peopleReducer.js
@@ -1,20 +1,47 @@
 import ActionTypes from '../../actions/actionTypes';
 import _ from 'lodash';
+import {combineReducers} from 'redux';
 
-const peopleReducer = (state = [], action) => {
+const peopleByNameReducer = (state = {}, action) => {
   switch (action.type) {
     case ActionTypes.RECEIVE_PEOPLE:
-      return [...state,
-              ...action.payload];
+      let newState = {...state};
+      _.each(action.payload, function(person) {
+        if (!state[person.name]) {
+          newState[person.name] = person;
+        }
+      });
+      return newState;
     default:
       return state;
   }
 };
 
+const allPeopleNamesReducer = (state = [], action) => {
+  switch (action.type) {
+    case ActionTypes.RECEIVE_PEOPLE:
+      let newState = [...state];
+      _.each(action.payload, function(person) {
+        if (!state.includes( person.name)) {
+          newState = [...newState,
+                      person.name];
+        }
+      });
+      return newState;
+    default:
+      return state;
+  }
+};
+
+const peopleReducer = combineReducers({
+  byName: peopleByNameReducer,
+  allNames: allPeopleNamesReducer
+});
+
 export {peopleReducer as default};
 
-export const getPeople = (state) => state;
+export const getPeople = (state) => state.allNames.map(name => state.byName[name]);
 
 export const getPerson = (state, personName) => {
-  return _.find(state, {name: personName});
+  return state.byName[personName];
 };
--- client/src/reducers/people/peopleReducer.spec.js
+++ client/src/reducers/people/peopleReducer.spec.js
@@ -2,12 +2,21 @@ import peopleReducer, {getPeople} from './peopleReducer';
 import peopleData from './people.json';
 import ActionTypes from '../../actions/actionTypes';
 import deepFreeze from 'deep-freeze';
+import _ from 'lodash';
 
 describe('PeopleReducer', function() {
   it('should have an array of people on RECEIVE_PEOPLE', function () {
-    let beforeState = [];
+    let beforeState = {};
     deepFreeze(beforeState);
     let afterState = peopleReducer(beforeState, {type: ActionTypes.RECEIVE_PEOPLE, payload: peopleData.results});
     expect(getPeople(afterState).length).toEqual(peopleData.results.length);
+  });
+
+  it('should not have duplicates on RECEIVE_PEOPLE', function() {
+    let beforeState = peopleReducer({}, {type: ActionTypes.RECEIVE_PEOPLE, payload: peopleData.results});
+    deepFreeze(beforeState);
+    let afterState = peopleReducer(beforeState, {type: ActionTypes.RECEIVE_PEOPLE, payload: peopleData.results});
+    let lukes = _.filter(getPeople(afterState), {name: 'Luke Skywalker'});
+    expect(lukes.length).toEqual(1);
   });
 });
